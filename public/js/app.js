8afa52d78789340c60bb8b141a5766ced92412af
Tom van Ommeren <tom.vanommeren@gmail.com>
2014-01-15 13:16:54 +0100

    add tour

--- public/js/app.js
+++ public/js/app.js
@@ -177,5 +177,51 @@ Mastermind = new function() {
 
 }();
 
+// Instance the tour
+var tour = new Tour();
+
+// Add your steps. Not too many, you don't really want to get your users sleepy
+tour.addSteps([
+  {
+    orphan: true, // string (jQuery selector) - html element next to which the step popover should be shown
+    title: "Play Mastermind!", // string - title of the popover
+    content: "Guess a 4-peg combination of 6 colors within 12 turns" // string - content of the popover
+  },
+  {
+    element: "#answer",
+    title: "The answer",
+    content: "Answers may contain repeating colors, but cannot be empty. Only the server knows the answer, so no peeking!"
+  },
+    {
+    element: "#addguess",
+    title: "Your guess",
+    content: "The first peg is selected, and you automatically advance to the next peg after choosing a color. You can click a peg to select a new color for it."
+  },
+    {
+    element: "#codepicker",
+    title: "Pick your colors",
+    content: "A single click places that color into your selected peg. The next peg is automatically selected"
+  },
+    {
+    element: "#addguessbutton",
+    title: "Add your guess",
+    content: "Once you have filled all four pegs, you can submit your guess."
+  },
+    {
+    orphan: true,
+    title: "Scoring",
+    content: "For each color in the right place, you receive a black peg. For each color that is in the wrong place, you receive a white peg. These pegs are not in the order of your guessed colors. Good luck!"
+  }
+
+]);
+
+// Initialize the tour
+tour.init();
+
+// Start the tour
+tour.start();
+
+$('#tourbutton').click(function() {tour.restart();});
+
 
     
